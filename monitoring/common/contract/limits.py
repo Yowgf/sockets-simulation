1fc43d9b56f5d7ea68c9baa97690434b6566ba88
yowgf <alexthomasmol@gmail.com>
2022-06-05 00:14:50 -0300

    Camera-ready version

--- monitoring/common/contract/limits.py
+++ monitoring/common/contract/limits.py
@@ -5,5 +5,3 @@ SENSOR_IDS = ['01', '02', '03', '04']
 
 MAX_NUM_SENSORS = 15
 
-def get_sensorid_from_int(sensorid_int):
-    return f"{sensorid_int:02d}"
--- monitoring/tests/conftest.py
+++ monitoring/tests/conftest.py
@@ -4,7 +4,7 @@ from ..client.client.client import Client
 from ..server.server.server import Server
 from .mocks import ClientConfig, ServerConfig
 
-PORT = 50131
+PORT = 51511
 
 @pytest.fixture
 def client():
